2f313cf92cdfed075e790b57edb25c1455be3d08
chris <christianwilliamsjire@gmail.com>
2019-09-05 15:45:21 +0300

    deployment to heroku

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key=70be95d2218fb04f5f456088e5b3c3b7'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+    export MOVIE_API_KEY=70be95d2218fb04f5f456088e5b3c3b7
+export SECRET_KEY=Flask WTF Secret Key
+
+python3.6 manage.py server
